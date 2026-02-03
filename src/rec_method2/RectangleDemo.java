14d9eb11f5fc6fc78fe6784032a539936be754ba
Caden Codes <cadenlcraddock@gmail.com>
2026-02-02 21:50:03 -0600

    Add files via upload

---
+++ src/rec_method2/RectangleDemo.java
@@ -0,0 +1,16 @@
+package rec_method2;
+
+public class RectangleDemo
+{
+    public static void main(String[] args)
+    {
+        rec_method2.Rectangle r2 = new Rectangle();
+        r2.setHeight(45);
+        r2.setLength(20);
+        System.out.println("The height of the rectangle is: " + r2.getHeight() + ".");
+        System.out.println("The height of the rectangle is: " + r2.getLength() + ".");
+
+        //R_method rr = new
+    }
+
+}
---
+++ src/rec_method2/RectangleUML.java
@@ -0,0 +1,17 @@
+package rec_method2;
+
+public class RectangleUML
+{
+    private double height;
+    private double length;
+    // Public methods
+    public void setHeight(double h)
+    {
+
+    }
+    public void setLength(double l)
+    {
+
+    }
+
+}
